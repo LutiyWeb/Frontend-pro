c53775f5533b37a3c7d0c6ab2f324499692ffbc9
Yuriy <Lutiy2501.com@gmail.com>
2023-04-28 16:53:52 +0300

    added last task

--- js-lessons2/script.js
+++ js-lessons2/script.js
@@ -1,16 +1,15 @@
-//Secondary homework
+//Thirst task
 userName = prompt('Hello! What is your name?');
 userAge = prompt('Please tell me how old are you');
 userSkills = prompt('Ok. What skills do you have');
 
 alert('So. Your name is ' + userName + '. ' + 'You are ' + userAge  + '.' + ' ' + 'And rou like to ' + userSkills);
 
-//Secondary homework
-
+//Secondary task
 userNumber = 25588;
 alert(userNumber.toString().replaceAll('', ' '));
 
-//Thirty
+Thirty
 thirstUserValue = +prompt('Plese enter thirst value');
 secondaryUserValue = +prompt('Plese enter secondary value');
 result = thirstUserValue + secondaryUserValue
@@ -20,9 +19,15 @@ minus = `${thirstUserValue} - ${secondaryUserValue} = ${result}`;
 multiply = `${thirstUserValue} * ${secondaryUserValue} = ${result}`;
 division = `${thirstUserValue} * ${secondaryUserValue} = ${result}`;
 
+//Third task
 alert(`
        ${sum}
        ${minus}
        ${multiply}
-       ${division  }
+       ${division}
        `)
+
+//Thours task
+hoursValue = +prompt('Enter plese random hours');
+secondaryValue = hoursValue * 60;
+alert(secondaryValue);
