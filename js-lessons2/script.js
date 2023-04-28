ec03eac082722db4cf5d388b8cdb3fb355eb21b9
Yuriy <Lutiy2501.com@gmail.com>
2023-04-28 13:01:45 +0300

    secondary home work

---
+++ js-lessons2/script.js
@@ -0,0 +1,15 @@
+//First homework
+userName = prompt('Hello! What is your name?');
+userAge = prompt('Please tell me how old are you');
+userSkills = prompt('Ok. What skills do you have');
+
+alert('So. Your name is ' + userName + '. ' + 'You are ' + userAge  + '.' + ' ' + 'And rou like to ' + userSkills);
+
+//Secondary homework
+firstValue = +prompt('your first value');
+secondaryValue = +prompt('your secondary value');
+
+sum = alert(firstValue + secondaryValue);
+substruction = alert(firstValue - secondaryValue);
+multiply = alert(firstValue * secondaryValue);
+division = alert(firstValue / secondaryValue);
