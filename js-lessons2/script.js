3f89be2ac91b30971ca51e6aaf7de56d6020b451
Yuriy <Lutiy2501.com@gmail.com>
2023-04-28 15:50:14 +0300

    secondary home work

--- js-lessons2/script.js
+++ js-lessons2/script.js
@@ -1,15 +1,18 @@
 //First homework
-userName = prompt('Hello! What is your name?');
-userAge = prompt('Please tell me how old are you');
-userSkills = prompt('Ok. What skills do you have');
+// userName = prompt('Hello! What is your name?');
+// userAge = prompt('Please tell me how old are you');
+// userSkills = prompt('Ok. What skills do you have');
 
-alert('So. Your name is ' + userName + '. ' + 'You are ' + userAge  + '.' + ' ' + 'And rou like to ' + userSkills);
+// alert('So. Your name is ' + userName + '. ' + 'You are ' + userAge  + '.' + ' ' + 'And rou like to ' + userSkills);
 
 //Secondary homework
-firstValue = +prompt('your first value');
-secondaryValue = +prompt('your secondary value');
 
-sum = alert(firstValue + secondaryValue);
-substruction = alert(firstValue - secondaryValue);
-multiply = alert(firstValue * secondaryValue);
-division = alert(firstValue / secondaryValue);
+userNumber = 25588;
+alert(userNumber.toString().replaceAll('', ' '))
+
+// userName = prompt('Please tell me your name');
+
+// editUserName = userName[0].toUpperCase();
+// otherUserName = userName.slice(1, userName.length)
+
+// console.log(`${editUserName}${otherUserName}`);
---
+++ js-lessons3/script.js
@@ -0,0 +1,7 @@
+firstValue = +prompt('your first value');
+secondaryValue = +prompt('your secondary value');
+
+sum = alert(firstValue + secondaryValue);
+substruction = alert(firstValue - secondaryValue);
+multiply = alert(firstValue * secondaryValue);
+division = alert(firstValue / secondaryValue);
