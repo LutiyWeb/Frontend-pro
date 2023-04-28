359b1a7661d42eea1d2e31e5b582399382e5a75c
Yuriy <Lutiy2501.com@gmail.com>
2023-04-28 15:51:25 +0300

    Correction

--- js-lessons2/script.js
+++ js-lessons2/script.js
@@ -1,18 +1,11 @@
-//First homework
-// userName = prompt('Hello! What is your name?');
-// userAge = prompt('Please tell me how old are you');
-// userSkills = prompt('Ok. What skills do you have');
+//Secondary homework
+userName = prompt('Hello! What is your name?');
+userAge = prompt('Please tell me how old are you');
+userSkills = prompt('Ok. What skills do you have');
 
-// alert('So. Your name is ' + userName + '. ' + 'You are ' + userAge  + '.' + ' ' + 'And rou like to ' + userSkills);
+alert('So. Your name is ' + userName + '. ' + 'You are ' + userAge  + '.' + ' ' + 'And rou like to ' + userSkills);
 
 //Secondary homework
 
 userNumber = 25588;
 alert(userNumber.toString().replaceAll('', ' '))
-
-// userName = prompt('Please tell me your name');
-
-// editUserName = userName[0].toUpperCase();
-// otherUserName = userName.slice(1, userName.length)
-
-// console.log(`${editUserName}${otherUserName}`);
