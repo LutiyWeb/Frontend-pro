14d7ccf033c0d27e3d06d058a45f163e0c9d9cbe
Yuriy <Lutiy2501.com@gmail.com>
2023-04-28 16:37:38 +0300

    homework

--- js-lessons2/script.js
+++ js-lessons2/script.js
@@ -8,4 +8,21 @@ alert('So. Your name is ' + userName + '. ' + 'You are ' + userAge  + '.' + ' ' 
 //Secondary homework
 
 userNumber = 25588;
-alert(userNumber.toString().replaceAll('', ' '))
+alert(userNumber.toString().replaceAll('', ' '));
+
+//Thirty
+thirstUserValue = +prompt('Plese enter thirst value');
+secondaryUserValue = +prompt('Plese enter secondary value');
+result = thirstUserValue + secondaryUserValue
+
+sum = `${thirstUserValue} + ${secondaryUserValue} = ${result}`;
+minus = `${thirstUserValue} - ${secondaryUserValue} = ${result}`;
+multiply = `${thirstUserValue} * ${secondaryUserValue} = ${result}`;
+division = `${thirstUserValue} * ${secondaryUserValue} = ${result}`;
+
+alert(`
+       ${sum}
+       ${minus}
+       ${multiply}
+       ${division  }
+       `)
