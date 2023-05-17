7ad3d17edc3d5f866ffa78139e3d1cb378cfa571
Yuriy <Lutiy2501.com@gmail.com>
2023-05-17 19:09:05 +0300

    homewok 4

---
+++ js-lessons4/script.js
@@ -0,0 +1,104 @@
+// Вивести на сторінку в один рядок через кому числа від 10 до 20.
+// count = 9;
+
+while (count >= 9 && count < 20) {
+    count++;
+    document.write(`
+                    ${count},
+                    `);
+}
+
+//Вивести квадрати чисел від 10 до 20
+// count = 9;
+// while (count >= 9 && count < 20) {
+//     count++;
+//     multiply = count * count;
+//     document.write(`
+//                     ${multiply},
+//                     `);
+// }
+
+
+//Вивести таблицю множення на 7.
+// firstValue = 7;
+// secondValue = 0;
+
+// while (secondValue < 10) {
+//     secondValue++
+//     multiply = firstValue * secondValue;
+//     document.write(`${multiply}, `)
+// }
+
+// Знайти суму всіх цілих чисел від 1 до 15.
+//  sum = 0;
+//  count = 0;
+
+//  while (count < 15) {
+//     count++;
+//     sum = sum + count;
+//     document.write(`${sum}, `)  
+//  }
+
+//Знайти добуток усіх цілих чисел від 15 до 35.
+// firstValue = 15;
+// secondValue = 15
+
+// while (secondValue <= 35) {
+//     secondValue++
+//     firstValue = firstValue * secondValue;
+//     document.write(`${firstValue}, `)
+// }
+
+//Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
+// i = 1;
+// sum = 0;
+// maxCount = 500
+// while (i <= maxCount) {
+//     i++
+//     sum += i;
+// }
+// document.write(`${sum / maxCount}`)
+
+//Вивести суму лише парних чисел в діапазоні від 30 до 80.
+// i = 30;
+// sum = 0
+// while (i <= 80) {
+//     i+=2
+//     sum += i
+//     document.write(`${sum}, `)
+// }
+
+//Вивести всі числа в діапазоні від 100 до 200 кратні 3.
+// i = 100;
+//  while (i <= 200) {
+//     i+=3
+//     console.log(i);
+//  }
+
+// Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
+// n = 12;
+// i = 0;
+// while (i < n) {
+//    if (n % i == 0) {
+//       document.write(`${i}, `)
+//    }
+//    i++;
+// }
+
+// Визначити кількість його парних дільників. //Не зрозумів як зробити
+// Знайти суму його парних дільників. //Не зрозумів як зробити
+
+
+// Надрукувати повну таблицю множення від 1 до 10.
+// i = 0;
+// while (i <= 9) {
+//    i++
+
+//    j = 1
+//    while (j <= 9) {
+//       j++
+//       res = i * j;
+//       document.write(`${res}, `)
+//    }
+   
+// }
